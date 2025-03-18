ade6fccfb2a092e854bb9e8efe6e2f9102977059
Samira Hellerová <samhell019@pslib.cz>
2025-03-18 20:48:28 +0100

    formular, tabulky, pozadi

--- potvrzeni.js
+++ potvrzeni.js
@@ -1,11 +1,26 @@
-// Funkce pro validaci formuláře
-document.getElementById('poptavkaForm').addEventListener('submit', function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('poptavkaForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Zabraňuje standardnímu odeslání formuláře
 
+        // Získání dat z formuláře
+        let jmeno = document.getElementById("jmeno").value;
+        let email = document.getElementById("email").value;
+        let destinace = document.getElementById("destinace").value;
+        let telefon = document.getElementById("telefon").value;
+        let predvolba = document.getElementById("predvolba").value;
 
-    // Pokud je validace OK, skrytí formuláře a zobrazení potvrzení
-    document.getElementById("poptavkaForm").style.display = "none";
-    document.getElementById("potvrzeni").style.display = "block";
+        // Zde můžete provést operace s těmito daty, pokud potřebujete
+
+        // Skrytí formuláře a zobrazení potvrzení
+        document.getElementById("poptavkaForm").style.display = "none"; // Skrytí formuláře
+        document.getElementById("potvrzeni").style.display = "block";  // Zobrazení potvrzení
+
+        // Možné další operace (např. validace dat, odeslání e-mailu, atd.)
+    });
 });
+
+
+
 
 // Funkce pro otevření/uzavření mobilního menu
 function toggleMenu() {
