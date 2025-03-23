298f24350783536712d0f80535426b956b91663d
Samira Hellerová <samhell019@pslib.cz>
2025-03-23 20:53:05 +0100

    finish

--- potvrzeni.js
+++ potvrzeni.js
@@ -17,26 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function toggleMenu() {
-    var menu = document.querySelector('ul');
-    menu.classList.toggle('open');
-}
-
-document.querySelectorAll('.dropdown').forEach(function(dropdown) {
-    dropdown.addEventListener('click', function(event) {
-        event.stopPropagation();
-        this.classList.toggle('open');
-    });
-});
-
-document.addEventListener('click', function(event) {
-    if (!event.target.closest('.dropdown')) {
-        document.querySelectorAll('.dropdown').forEach(function(dropdown) {
-            dropdown.classList.remove('open');
-        });
-    }
-});
-
 function printTable() {
     let originalContent = document.body.innerHTML;
     let tableContent = document.getElementById("letenkyTable").outerHTML;
